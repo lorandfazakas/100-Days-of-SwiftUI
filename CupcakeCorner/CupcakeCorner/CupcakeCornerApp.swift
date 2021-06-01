42e73998ae1ec592cf6d9b02bc1ca5522cad8d09
Lorand Fazakas <lorando.fazakas@gmail.com>
2021-06-01 13:29:20 +0200

    project 10 with challenge 1,2

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Lorand Fazakas on 2021. 05. 31..
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
